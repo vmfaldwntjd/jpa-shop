7fca8507245ba5a251460ed5e5244f2c8cc609da
core <jinjs6@gmail.com>
2023-03-17 12:40:02 +0900

    test: 변경 감지 기능 테스트

---
+++ src/test/java/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void updateTest() {
+        Book book = em.find(Book.class, 1L);
+
+        //dirty checking
+        book.setName("updateName");
+    }
+}
